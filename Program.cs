dcfc0427c9c26694d7dd220f667ec79800187781
MiqueasReyes <miqueasreyescruz@gmail.com>
2026-05-23 20:29:46 -0300

    Finalizo el condigo y funcionamiento de CalculadoraV1

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 
-int selector, num1, num2, result;
+float num1 = 0, num2 = 0, result = 0;
+string selector = "";
 bool verificador = true;
 
 
@@ -12,67 +13,75 @@ do
     Console.WriteLine("2. RESTA");
     Console.WriteLine("3. MILTIPLICACION");
     Console.WriteLine("4. DIVISION");
-    Console.WriteLine("5. SALIR");
+    Console.WriteLine("------------------");
+
+    selector = Console.ReadLine();
+    if (selector != "1" && selector != "2" && selector != "3" && selector != "4")
+    {
+        Console.WriteLine("-> ERROR: opcion no valida, ingrese nuevamente:");
+    }
+    else
+    {
+        bool validarNum1 = false, validarNum2 = false;
+        string aux = "";
+
+        while(!validarNum1)
+        {
+            Console.WriteLine("-> Ingrese el primer numero: ");
+            validarNum1 = float.TryParse(aux, out num1);
+            if (!validarNum1) Console.WriteLine("ERROR: valor invalido ingresado");
+        }
+
+        while(!validarNum2)
+        {
+            Console.WriteLine("-> Ingrese el segundo numero: ");
+            aux = Console.ReadLine();
+            validarNum2 = float.TryParse(aux, out num2);
+            if (!validarNum2) Console.WriteLine("ERROR: valor invalido ingresado");
+        }
+
+        switch (selector)
+        {
+            case "1":
+                result = num1 + num2;
+                Console.WriteLine(num1 + " + " + num2 + " = " + result);
+                break;
+            case "2":
+                result = num1 - num2;
+                Console.WriteLine(num1 + " - " + num2 + " = " + result);
+                break;
+            case "3":
+                result = num1 * num2;
+                Console.WriteLine(num1 + " * " + num2 + " = " + result);
+                break;
+            case "4":
+                if(num2 != 0)
+                {
+                    result = num1 / num2;
+                    Console.WriteLine(num1 + " + " + num2 + " = " + result);   
+                }
+                else
+                {
+                    Console.Write("ERROR: no se puede divir en 0");
+                }
+                break;
+            default:
+                Console.WriteLine("ERROR: valor no esperado");
+                break;
+        }
+    }
+
     Console.WriteLine("------------------");
 
     do
     {
-        selector = Console.Read();
-        if (selector < 1 || selector > 5)
-        {
-            Console.WriteLine("-> ERROR: opcion no valida, ingrese nuevamente:");
-        }
-    } while(selector < 1 || selector > 5);
+        Console.WriteLine("-> Desea realizar otra operacion? (S/N)");
+        selector = Console.ReadLine().ToUpper();
+    } while (selector != "S" && selector != "N");
 
-    Console.WriteLine("-> Ingrese el primer numero: ");
-    num1 = Console.Read();
-
-    Console.WriteLine("-> Ingrese el segundo numero: ");
-    num2 = Console.Read();
-
-    switch (selector)
-    {
-        case 1:
-            result = num1 + num2;
-            Console.WriteLine(num1 + " + " + num2 + " = " + result);
-            break;
-        case 2:
-            result = num1 - num2;
-            Console.WriteLine(num1 + " - " + num2 + " = " + result);
-            break;
-        case 3:
-            result = num1 * num2;
-            Console.WriteLine(num1 + " * " + num2 + " = " + result);
-            break;
-        case 4:
-            if(num2 != 0)
-            {
-                result = num1 / num2;
-                Console.WriteLine(num1 + " + " + num2 + " = " + result);   
-            }
-            else
-            {
-                Console.Write("ERROR: no se puede divir en 0");
-            }
-            break;
-        case 5:
-            Console.WriteLine("Saliendo...");
-            break;
-        default:
-            Console.WriteLine("ERROR: valor no esperado");
-            break;
-    }
-
-    do
-    {
-        Console.WriteLine("-> Desea realizar otra operacion? (1. SI / 2. NO)");
-        selector = Console.Read();
-    } while (selector < 1 || selector > 2);
-
-    if (selector == 2)
+    if (selector == "N")
     {
         verificador = false;
         Console.WriteLine("Saliendo...");
     }
-
 } while (verificador);
