d1f9fe87bde4114dc1d4273275de48fe364b89ac
MiqueasReyes <miqueasreyescruz@gmail.com>
2026-05-23 19:57:02 -0300

    prototipo del CalculadoraV1, errores con el tipo de variable

--- Program.cs
+++ Program.cs
@@ -1,19 +1,78 @@
-﻿int num = 0, aux = 0;
-string s = "";
+﻿using System;
 
-Console.WriteLine("- Ingrese el numero a invertir: ");
-s = Console.ReadLine();
+int selector, num1, num2, result;
+bool verificador = true;
 
-if (int.TryParse(s, out num))
+
+Console.WriteLine("--- CalculadoraV1 ---");
+do
 {
-    while (num > 0)
+    Console.WriteLine("------ MENU ------");
+    Console.WriteLine("1. SUMA");
+    Console.WriteLine("2. RESTA");
+    Console.WriteLine("3. MILTIPLICACION");
+    Console.WriteLine("4. DIVISION");
+    Console.WriteLine("5. SALIR");
+    Console.WriteLine("------------------");
+
+    do
     {
-        aux = aux * 10 + num % 10;
-        num = num / 10;
+        selector = Console.Read();
+        if (selector < 1 || selector > 5)
+        {
+            Console.WriteLine("-> ERROR: opcion no valida, ingrese nuevamente:");
+        }
+    } while(selector < 1 || selector > 5);
+
+    Console.WriteLine("-> Ingrese el primer numero: ");
+    num1 = Console.Read();
+
+    Console.WriteLine("-> Ingrese el segundo numero: ");
+    num2 = Console.Read();
+
+    switch (selector)
+    {
+        case 1:
+            result = num1 + num2;
+            Console.WriteLine(num1 + " + " + num2 + " = " + result);
+            break;
+        case 2:
+            result = num1 - num2;
+            Console.WriteLine(num1 + " - " + num2 + " = " + result);
+            break;
+        case 3:
+            result = num1 * num2;
+            Console.WriteLine(num1 + " * " + num2 + " = " + result);
+            break;
+        case 4:
+            if(num2 != 0)
+            {
+                result = num1 / num2;
+                Console.WriteLine(num1 + " + " + num2 + " = " + result);   
+            }
+            else
+            {
+                Console.Write("ERROR: no se puede divir en 0");
+            }
+            break;
+        case 5:
+            Console.WriteLine("Saliendo...");
+            break;
+        default:
+            Console.WriteLine("ERROR: valor no esperado");
+            break;
     }
-    Console.WriteLine("- El numero invertido del numero ingresado es: " + aux);
-}
-else
-{
-    Console.WriteLine("- Error: El valor ingresado no es un numero");
-}
+
+    do
+    {
+        Console.WriteLine("-> Desea realizar otra operacion? (1. SI / 2. NO)");
+        selector = Console.Read();
+    } while (selector < 1 || selector > 2);
+
+    if (selector == 2)
+    {
+        verificador = false;
+        Console.WriteLine("Saliendo...");
+    }
+
+} while (verificador);
