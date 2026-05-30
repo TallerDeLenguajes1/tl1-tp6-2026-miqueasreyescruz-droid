d07ff7069f40f9f9e40d7d47f301a4946efa8b34
MiqueasReyes <miqueasreyescruz@gmail.com>
2026-05-29 22:57:41 -0300

    Agrego la funcion maximo que compara numero y termino la implementacion

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-double num1 = 0, num2 = 0, result = 0;
+double num1 = 0, num2 = 0, num3 = 0, result = 0;
 string selector = "";
 bool verificador = true;
 
@@ -25,7 +25,7 @@ do
         }
     } while (selector != "1" && selector != "2" && selector != "3" && selector != "4" && selector != "5");
     
-    bool validarNum1 = false, validarNum2 = false;
+    bool validarNum1 = false, validarNum2 = false, validarNum3 = false;
 
     if (selector != "5")
     {
@@ -70,13 +70,37 @@ do
             }
             break;
         case "5":
-            // Falta ingresar el numero
-            Console.WriteLine("-> Analizando el numero " + num1 + ":");
-            Console.WriteLine("Valor Absoluto: " + valorAbs(num1));
-            Console.WriteLine("Cuadrado: " + cuadrado(num1));
-            Console.WriteLine("Razi cuadrada: " + raizCuadrada(num1));
-            Console.WriteLine("Seno: " + seno(num1));
-            Console.WriteLine("Coseno: " + coseno(num1));
+            while(!validarNum3)
+            {
+                Console.WriteLine("-> Ingrese el numero a analizar: ");
+                validarNum3 = double.TryParse(Console.ReadLine(), out num3);
+                if (!validarNum3) Console.WriteLine("ERROR: valor invalido ingresado");
+            }
+
+            Console.WriteLine("-> Analizando el numero " + num3 + ":");
+            Console.WriteLine("Valor Absoluto: " + valorAbs(num3));
+            Console.WriteLine("Cuadrado: " + cuadrado(num3));
+            Console.WriteLine("Razi cuadrada: " + raizCuadrada(num3));
+            Console.WriteLine("Seno: " + seno(num3));
+            Console.WriteLine("Coseno: " + coseno(num3));
+            Console.WriteLine("------------------");
+            
+            Console.WriteLine("COMPARANDO NUMEROS");
+            while(!validarNum1)
+            {
+                Console.WriteLine("-> Ingrese el primer numero: ");
+                validarNum1 = double.TryParse(Console.ReadLine(), out num1);
+                if (!validarNum1) Console.WriteLine("ERROR: valor invalido ingresado");
+            }
+
+            while(!validarNum2)
+            {
+                Console.WriteLine("-> Ingrese el segundo numero: ");
+                validarNum2 = double.TryParse(Console.ReadLine(), out num2);
+                if (!validarNum2) Console.WriteLine("ERROR: valor invalido ingresado");
+            }
+
+            maximo(num1,num2);
             break;
         default:
             Console.WriteLine("ERROR: valor no esperado");
@@ -114,7 +138,16 @@ double cuadrado (double num)
 
 double raizCuadrada (double num)
 {
-    num = Math.Sqrt(num);
+    if (num >= 0)
+    {
+        num = Math.Sqrt(num);    
+    }
+    else
+    {
+        Console.WriteLine("Raiz no existente en el conjunto de los numeros Reales");
+        num = 0;
+    }
+    
     return num;
 }
 
@@ -130,4 +163,16 @@ double coseno (double num)
     double radianes = num * (Math.PI / 180.0);
     num = Math.Cos(radianes); 
     return num;
+}
+
+void maximo (double num1, double num2)
+{
+    if (num1 > num2)
+    {
+        Console.WriteLine("El numero " + num1 + " es el mayor, y el numero " + num2 + " es el menor");
+    }
+    else
+    {
+        Console.WriteLine("El numero " + num2 + " es el mayor, y el numero " + num1 + " es el menor");
+    }
 }
