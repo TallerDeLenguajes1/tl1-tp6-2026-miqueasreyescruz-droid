b171a6bf73577cc3bce024869e8b75a75f57bdc9
MiqueasReyes <miqueasreyescruz@gmail.com>
2026-06-06 16:54:09 -0300

    Realizo el punto 4, previo a agregar la Calculadora

--- Program.cs
+++ Program.cs
@@ -1,19 +1,33 @@
-﻿int num = 0, aux = 0;
-string s = "";
+﻿// int num = 0, aux = 0;
+// string s = "";
 
-Console.WriteLine("- Ingrese el numero a invertir: ");
-s = Console.ReadLine();
+// Console.WriteLine("- Ingrese el numero a invertir: ");
+// s = Console.ReadLine();
 
-if (int.TryParse(s, out num))
-{
-    while (num > 0)
-    {
-        aux = aux * 10 + num % 10;
-        num = num / 10;
-    }
-    Console.WriteLine("- El numero invertido del numero ingresado es: " + aux);
-}
-else
-{
-    Console.WriteLine("- Error: El valor ingresado no es un numero");
-}
+// if (int.TryParse(s, out num))
+// {
+//     while (num > 0)
+//     {
+//         aux = aux * 10 + num % 10;
+//         num = num / 10;
+//     }
+//     Console.WriteLine("- El numero invertido del numero ingresado es: " + aux);
+// }
+// else
+// {
+//     Console.WriteLine("- Error: El valor ingresado no es un numero");
+// }
+
+string cadena1, cadena2;
+
+Console.WriteLine("Ingrese una cadena de texto:: ");
+cadena1 = Console.ReadLine();
+
+Console.WriteLine($"Longitud de la cadena ingresada: {cadena1.Length}");
+
+Console.WriteLine("Ingrese una segunda cadena de texto: ");
+cadena2 = Console.ReadLine();
+
+Console.WriteLine($"Resultado de la concatenacion de ambas cadenas: {cadena1}{cadena2}");
+
+Console.WriteLine($"Subcadena extraida: {cadena1.Substring(0, (cadena1.Length / 2))}");
